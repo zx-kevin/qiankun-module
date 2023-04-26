d79d03108ff9a3f344e923d10f1a426138d0574d
文振雄 <wenzhenxiong@jeoho.com>
2023-04-26 18:30:22 +0800

    ✨ feat: 路由配置调整

--- src/permission.js
+++ src/permission.js
@@ -1,89 +1,147 @@
-import router from './router'
-import { ElMessage } from 'element-plus'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import { getToken } from '@/utils/auth'
-import { isHttp } from '@/utils/validate'
-import { isRelogin } from '@/utils/request'
-import useUserStore from '@/store/modules/user'
-import useSettingsStore from '@/store/modules/settings'
-import usePermissionStore from '@/store/modules/permission'
+import router from './router';
+import { ElMessage } from 'element-plus';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { getToken } from '@/utils/auth';
+import { isHttp } from '@/utils/validate';
+import { isRelogin } from '@/utils/request';
+import useUserStore from '@/store/modules/user';
+import useSettingsStore from '@/store/modules/settings';
+import usePermissionStore from '@/store/modules/permission';
 
 import useAppStore from '@/store/modules/app.js';
 import { getDomainApi } from '@/api/module/chat/im/chatGroup.js';
 
-import { qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
-import hosts from '@/hosts'
+import PortalView from '@/layout/portalView';
+import { qiankunWindow } from 'vite-plugin-qiankun/dist/helper';
+import hosts, { getHost } from '@/hosts';
 
 NProgress.configure({ showSpinner: false });
 
 const whiteList = ['/login', '/auth-redirect', '/bind', '/register'];
 
 router.beforeEach((to, from, next) => {
-  NProgress.start()
+  NProgress.start();
   if (getToken()) {
-    to.meta.title && useSettingsStore().setTitle(to.meta.title)
+    to.meta.title && useSettingsStore().setTitle(to.meta.title);
     /* has token*/
     if (to.path === '/login') {
-      next({ path: '/' })
-      NProgress.done()
+      next({ path: '/' });
+      NProgress.done();
     } else {
       if (useUserStore().roles.length === 0) {
-        isRelogin.show = true
+        isRelogin.show = true;
         // 判断当前用户是否已拉取完user_info信息
-        useUserStore().getInfo().then(() => {
-          isRelogin.show = false
-          usePermissionStore().generateRoutes().then(accessRoutes => {
-            // 根据roles权限生成可访问的路由表
-            accessRoutes.forEach(route => {
-              //  路由通配调整
-              if (!qiankunWindow.__POWERED_BY_QIANKUN__) handlePathMatch(route)
-              if (!isHttp(route.path)) {
-                router.addRoute(route) // 动态添加可访问路由表
-              }
-            })
-            next({ ...to, replace: true }) // hack方法 确保addRoutes已完成
+        useUserStore()
+          .getInfo()
+          .then(() => {
+            isRelogin.show = false;
+            usePermissionStore()
+              .generateRoutes()
+              .then((accessRoutes) => {
+                // 根据roles权限生成可访问的路由表
+                routeNameHandler(accessRoutes);
+                accessRoutes.forEach((route) => {
+                  //  路由通配调整
+                  const translateRoutes = handlePathMatch(route);
+                  translateRoutes.forEach((item) => {
+                    if (!isHttp(item.path)) {
+                      router.addRoute(item); // 动态添加可访问路由表
+                    }
+                  });
+                });
+                next({ ...to, replace: true }); // hack方法 确保addRoutes已完成
+              });
+            useUserStore().getFileDomain();
           })
-          useUserStore().getFileDomain()
-          getDomainApi().then((res) => {
-            // let { chatDomain, videoDomain } = res.data;
-            useAppStore().changeDomain(res.data);
+          .catch((err) => {
+            useUserStore()
+              .logOut()
+              .then(() => {
+                ElMessage.error(err);
+                next({ path: '/' });
+              });
           });
-        }).catch(err => {
-          useUserStore().logOut().then(() => {
-            ElMessage.error(err)
-            next({ path: '/' })
-          })
-        })
       } else {
-        next()
+        next();
       }
     }
   } else {
     // 没有token
     if (whiteList.indexOf(to.path) !== -1) {
       // 在免登录白名单，直接进入
-      next()
+      next();
     } else {
-      next(`/login?redirect=${to.fullPath}`) // 否则全部重定向到登录页
-      NProgress.done()
+      next(`/login?redirect=${to.fullPath}`); // 否则全部重定向到登录页
+      NProgress.done();
     }
   }
-})
+});
 
 router.afterEach(() => {
-  NProgress.done()
-})
+  NProgress.done();
+});
 
-const hostKeys = Object.keys(hosts)
-const handlePathMatch = (route) => {
-  const { path, children } = route
-  if (path !== '/' && !hostKeys.includes(pathSplit(path)) || path === '/' && !(children || []).filter(child => hostKeys.includes(pathSplit(child.path))).length) return
+const hostKeys = Object.keys(hosts);
 
-  const targetChildren = (children || []).filter(child => path == '/' ? hostKeys.includes(pathSplit(child.path)) : true)
-  for (const item of targetChildren) {
-    item.path = item.path + `${item.path.slice(-1) !== '/' ? '/' : ''}` + ':pathMatch(.*)*'
+/**
+ * @description: 路由名字处理
+ * @param {*} routes
+ * @param {*} name
+ * @return {*}
+ */
+function routeNameHandler(routes = [], name = '') {
+  for (const item of routes) {
+    if (name) item.name = `${name}-${item.name}`;
+    routeNameHandler(item.children, item.name);
   }
 }
 
-const pathSplit = (url) => url.split('/').filter(str => str)[0] || ''
+/**
+ * @description: 路由处理
+ * @param {*} route 目标路由
+ * @param {*} url   父路由路径
+ * @return {*}  路由数组
+ */
+const handlePathMatch = (route, url = '') => {
+  const { path, children, component } = route;
+  const result = [];
+  if (!route) return result;
+
+  const urlSplit = (target) =>
+    `/${target
+      .split('/')
+      .filter((str) => str)
+      .join('/')}`; //  url 处理 / 重复
+
+  if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+    //  父应用根据 hosts 配置 调整对应路由指向 pathMatch(.*)*
+    const isHost =
+      (path !== '/' && !hostKeys.includes(pathSplit(path))) ||
+      (path === '/' &&
+        !(children || []).filter((child) => hostKeys.includes(pathSplit(child.path))).length); //  判断匹配 hosts 配置
+    const targetChildren = (children || []).filter((child) =>
+      path == '/' ? hostKeys.includes(pathSplit(child.path)) : true,
+    );
+    for (const item of targetChildren) {
+      item.path = isHost
+        ? item.path + `${item.path.slice(-1) !== '/' ? '/' : ''}` + ':pathMatch(.*)*'
+        : item.path;
+    }
+
+    return [route];
+  } else {
+    // 子应用跳过 layout 布局 直接加载页面
+    if (children && children.length) {
+      for (const child of children) {
+        result.push(...handlePathMatch(child, urlSplit(url + '/' + path)));
+      }
+    } else {
+      result.push({ ...route, path: urlSplit(url + '/' + path) });
+    }
+  }
+
+  return result;
+};
+
+const pathSplit = (url) => url.split('/').filter((str) => str)[0] || '';
